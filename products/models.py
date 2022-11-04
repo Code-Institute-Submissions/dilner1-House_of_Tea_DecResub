5923a6a125b29149253ce411557f1edeb7ec6e53
dilner1 <dan.milner871@gmail.com>
2022-11-04 17:51:43 +0000

    Adding categories and products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=50, null=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    price = models.FloatField()
+    info = models.TextField()
+    category = models.ForeignKey('Categories', null=True, blank=True, on_delete=models.SET_NULL)
+    image = models.ImageField()
+    image_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
