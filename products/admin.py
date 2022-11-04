5bf6a3d1cde94727d85355cecd8fde1e305f11a3
dilner1 <dan.milner871@gmail.com>
2022-11-04 19:58:03 +0000

    Adding display sku in product admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Categories, Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+        list_display = (
+            'sku',
+            'name',
+        )
+
 admin.site.register(Categories)
 admin.site.register(Product)
