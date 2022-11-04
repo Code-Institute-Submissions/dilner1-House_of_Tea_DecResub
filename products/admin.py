055cae0116be7931a03ad2251ec4f74f4c6e1aba
dilner1 <dan.milner871@gmail.com>
2022-11-04 18:28:50 +0000

    Importing models into product admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from .models import Categories, Product
 
 # Register your models here.
-admin.site.register(Caterories)
-admin.site.register(Products)
+admin.site.register(Categories)
+admin.site.register(Product)
