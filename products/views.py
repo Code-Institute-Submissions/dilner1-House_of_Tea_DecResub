48fbd79728cf176dbbf3baebc1a54eb52811832e
dilner1 <dan.milner871@gmail.com>
2022-11-05 02:44:30 +0000

    Adding category filter to productViews

--- products/views.py
+++ products/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Categories
+from django.db.models import Q
 
 # Create your views here.
 def productsView(request):
 
+    query = None
+    category = None
     products = Product.objects.all()
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=cateroies)
+            categories = Categories.objects.filter(name__in=categories)
+
+
 
     context = {
         'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
