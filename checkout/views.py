39ff61a9960ff77fb0ee13c1c5be37557388c7c0
dilner1 <dan.milner871@gmail.com>
2022-11-17 23:01:39 +0000

    Updating checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 from .models import Order
+from .forms import OrderForm
 
 def checkoutView(request):
     """
-        This view loads the index page
+        This view loads the checkout page
     """
-    
-    return render(request, 'checkout/checkout.html')
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, 'Oops, looks like there is nothing in your basket yet.')
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    comtext = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
