5470d38b33871dae06ddfc972d3118e1dbec9d2d
dilner1 <dan.milner871@gmail.com>
2022-11-17 22:44:25 +0000

    Customising order form

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,2 +1,44 @@
 from django import forms
 from .models import Order
+
+class orderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'name',
+            'email',
+            'phone_number',
+            'country',
+            'postcode',
+            'city',
+            'address1',
+            'address2',
+            'county',
+        )
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': ' Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'City': 'Town or City',
+            'address1': 'Street Address 1',
+            'address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
