1318a0fce70107753bd7cbae5668cdff36e924da
dilner1 <dan.milner871@gmail.com>
2022-11-24 23:37:18 +0000

    Creating suppliers model

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Suppliers(models.Model):
+    supplier_name = models.CharField(max_length=60, null=True)
+    location = models.CharField(max_length=60, null=True)
+    info = models.TextField()
+    image = models.ImageField()
+    image_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.supplier_name
