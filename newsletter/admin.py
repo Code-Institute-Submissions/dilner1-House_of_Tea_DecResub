3346800424e6dcba647f52e6f7e9c58724c6d7f5
dilner1 <dan.milner871@gmail.com>
2022-11-24 23:16:46 +0000

    Registering Newsletter model in admin.py

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Newsletter
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    model = OrderLineItems
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
