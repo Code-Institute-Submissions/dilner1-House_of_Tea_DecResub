9e5d90e6c6664c7af5c8395b03e7dd4c3e3f23e6
dilner1 <dan.milner871@gmail.com>
2022-11-26 13:40:17 +0000

    Creating form view in newsletter

---
+++ newsletter/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Newsletter
+
+
+class newsletterForm(forms.Form):
+    class Meta:
+        model = Newsletter
+        fields = (
+            'name',
+            'email',
+        )
+        def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'name': ' Name',
+                'email': 'Email Address',
+            }
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from .forms import newsletterForm
 
 
 def newsletterView(request):
     """
         This view loads the newsletter signup form
     """
+    newsletter_form = newsletterForm()
+    if request.method == 'POST':
 
-    return render(request, 'newsletter/newsletter.html')
+        form_data = {
+            'name': request.POST['name'],
+            'email': request.POST['email'],
+        }
+        newsletter_form = newsletterForm(form_data)
+        if newsletter_form.is_valid():
+            print("Form is valid")
+            newsletter_form.save()
+
+    context = {
+        'newsletter_form': newsletter_form,
+    }
+
+    return render(request, 'newsletter/newsletter.html', context)
