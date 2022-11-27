858ced91d7aaae3ecf059d8f0536970282deafdd
dilner1 <dan.milner871@gmail.com>
2022-11-27 07:56:42 +0000

    amending database

--- house_of_tea/settings.py
+++ house_of_tea/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'house-of-tea.onrender.com']
 
@@ -118,16 +118,16 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-     'default': dj_database_url.parse('postgres://ewuqxztt:q6THtDhBQaU8mng4TT-Pt9HVWhzTaLKY@surus.db.elephantsql.com/ewuqxztt')
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#      'default': dj_database_url.parse('postgres://ewuqxztt:q6THtDhBQaU8mng4TT-Pt9HVWhzTaLKY@surus.db.elephantsql.com/ewuqxztt')
+#  }
 
 
 # Password validation
