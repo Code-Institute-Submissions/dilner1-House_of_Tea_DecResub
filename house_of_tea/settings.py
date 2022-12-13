6ee248aeaf0c72531e2270b8390b874be0ebe082
dilner1 <dan.milner871@gmail.com>
2022-12-13 12:05:48 +0000

    Removing commented out environment variable

--- house_of_tea/settings.py
+++ house_of_tea/settings.py
@@ -116,10 +116,6 @@ ACCOUNT_USERNAME_MIN_LENGTH = 5
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-# DATABASES = {
-#      'default': dj_database_url.parse('postgres://ewuqxztt:q6THtDhBQaU8mng4TT-Pt9HVWhzTaLKY@surus.db.elephantsql.com/ewuqxztt')  # noqa
-#  }
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ
@@ -190,3 +186,15 @@ DELIVERY_CHARGE_MAX = 50
 
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'theteahousenewsletter@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
